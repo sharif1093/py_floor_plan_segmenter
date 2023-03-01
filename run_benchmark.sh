85c5c3d2acdeb78b7cc26a348d747562fa94dd6e
Mohammadreza Sharif <mohammadreza.sharif@neatorobotics.com>
2023-02-28 16:07:43 -0800

    Optional debug mode in benchmark

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+DEBUG=${2:-"--debug"}
+
 for d in sandbox/maps/$1/*/ ; do
     echo "$d"
-    python -m py_floor_plan_segmenter -c py_floor_plan_segmenter/benchmark.yml -i $d -p sandbox/out/$1 --debug
+    python -m py_floor_plan_segmenter -c py_floor_plan_segmenter/benchmark.yml -i $d -p sandbox/out/$1 ${DEBUG}
     echo "============================"
     echo
 done
